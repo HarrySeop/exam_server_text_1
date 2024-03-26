9a7d2486b4e604908ba50b5df28085555b371d73
HarrySeop <parkpjs3083@naver.com>
2024-03-26 22:26:54 +0900

    five

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ package a202114154;
 public class Main {
 
   public static void main(String[] args) {
+    Five.print();
     Two.print();
     System.out.println("one 202114154");
     Three.print();
